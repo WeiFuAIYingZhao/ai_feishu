f8f51d41c644b665f0c28f58d0cb7f4ac16c141a
wei.fu <fuwei89409988@gmail.com>
2024-09-12 16:27:41 +0800

    更新目录

--- ai_app/settings.py
+++ ai_app/settings.py
@@ -1,9 +1,9 @@
 #  飞书认证配置
 FEISHU_V2_SDK_CONFIGS = {
-    'FEISHU_APP_ID': "",  # 企业应用的id
-    'FEISHU_APP_SECRET': "",  # 企业应用的SECRET
-    'FEISHU_VERIFY_TOKEN': "",  # 企业应用的事件加密token-------->
-    'FEISHU_ENCRYPT_KEY': "",  # 企业应用的事件加密key
+    'FEISHU_APP_ID': "cli_a4f0bdfed979100d",  # 企业应用的id
+    'FEISHU_APP_SECRET': "e807nr7TfBpFMnjKi2PnFe0CNitgvDiL",  # 企业应用的SECRET
+    'FEISHU_VERIFY_TOKEN': "tepxqDu7vHjh5pKUPfiaRcZY6R5uFrFb",  # 企业应用的事件加密token-------->
+    'FEISHU_ENCRYPT_KEY': "XCI6oo1coO6clOweZYlxIf8zjfqH7mVD",  # 企业应用的事件加密key
 }
 
 
